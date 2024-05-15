747b2c3dd4b0b80bd2615a5cc463ee4ec4d035d1
Jonnie-Dev <jtech2096@gmail.com>
2024-05-15 13:50:01 +0100

    feat: disabled playback button and link on faq image

--- components/Accordion/Accordion.jsx
+++ components/Accordion/Accordion.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import faq from '@/public/images/faq.jpg'
+import faq from '@/public/images/faq.png'
 import { LightboxVideo } from '..';
 
 const Accordion = ({ data }) => {
